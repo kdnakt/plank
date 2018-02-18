8913b63e88f57f0621b33a25fb8c09c20c0dbe49
kdnakt <a.kid.1985@gmail.com>
2018-02-19 03:00:48 +0900

    uninstall stopwatch-timer

--- src/TimerScreen.js
+++ src/TimerScreen.js
@@ -6,10 +6,6 @@ import {
   TouchableHighlight,
 } from 'react-native';
 import ReactNativeBgTimer from 'react-native-background-timer';
-import {
-  Stopwatch,
-  Timer
-} from 'react-native-stopwatch-timer';
 
 type State = {
   time: null,
@@ -19,19 +15,11 @@ class TimerScreen extends Component<State> {
 
   constructor() {
     super();
-    ReactNativeBgTimer.runBackgroundTimer(
-      () => this._countUp(),
-      1000);
     this.state = {
       timeCount: 1,
-      timerStart: false,
       stopwatchStart: false,
-      totalDuration: 90000,
-      timerReset: false,
       stopwatchReset: false,
     };
-    this.toggleTimer = this.toggleTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
     this.toggleStopwatch = this.toggleStopwatch.bind(this);
     this.resetStopwatch = this.resetStopwatch.bind(this);
   }
@@ -44,24 +32,18 @@ class TimerScreen extends Component<State> {
     console.log(timeCount);
   }
 
-  toggleTimer() {
-    this.setState({
-      timerStart: !this.state.timerStart,
-      timerReset: false,
-    });
-  }
-
-  resetTimer() {
-    this.setState({
-      timerStart: false,
-      timerReset: true,
-    });
-  }
-
   toggleStopwatch() {
     this.setState({
       stopwatchStart: !this.state.stopwatchStart,
       stopwatchReset: false,
+    }, () => {
+      if (this.state.stopwatchStart) {
+        ReactNativeBgTimer.runBackgroundTimer(
+          () => this._countUp(),
+          1000);
+      } else {
+        ReactNativeBgTimer.stopBackgroundTimer();
+      }
     });
   }
 
@@ -72,47 +54,23 @@ class TimerScreen extends Component<State> {
     });
   }
 
-  getFormattedTime(time) {
-    this.currentTime = time;
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>
-          {'This is TimerScreen' + this.state.timeCount}
+          {'This is TimerScreen: ' + this.state.timeCount}
         </Text>
-        <Stopwatch laps msecs
-          start={this.state.stopwatchStart}
-          reset={this.state.stopwatchReset}
-          options={options}
-          getTime={this.getFormattedTime} />
+
         <TouchableHighlight onPress={this.toggleStopwatch}>
-          <Text style={options.text}>{!this.state.stopwatchStart ? "Start" : "Stop"}</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this.resetStopwatch}>
-          <Text style={options.text}>Reset</Text>
-        </TouchableHighlight>
-        <Timer msecs
-          totalDuration={this.state.totalDuration}
-          start={this.state.timerStart}
-          reset={this.state.timerReset}
-          options={options}
-          handleFinish={handleTimerComplete}
-          getTime={this.getFormattedTime} />
-        <TouchableHighlight onPress={this.toggleTimer}>
-          <Text style={options.text}>{!this.state.timerStart ? "Start" : "Stop"}</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this.resetTimer}>
-          <Text style={options.text}>Reset</Text>
+          <Text style={options.text}>
+            {!this.state.stopwatchStart ? "Start" : "Stop"}
+          </Text>
         </TouchableHighlight>
       </View>
     );
   }
 
 }
-
-const handleTimerComplete = () => alert("custom completion function");
 
 const options = {
   container: {
