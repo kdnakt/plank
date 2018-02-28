4d1f1ea342b3930e49055183aa48e516335b6301
kdnakt <a.kid.1985@gmail.com>
2018-03-01 07:45:47 +0900

    install and link realm

--- android/app/src/main/java/com/plank/MainApplication.java
+++ android/app/src/main/java/com/plank/MainApplication.java
@@ -3,6 +3,7 @@ package com.plank;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new VectorIconsPackage(),
             new VectorIconsPackage(),
             new BackgroundTimerPackage(),
